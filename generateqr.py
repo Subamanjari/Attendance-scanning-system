3c181d58c2eb37e7c06f798ad3d27bd6bcff89f7
Subamanjari <manjumlr30@gmail.com>
2025-08-05 11:48:43 +0530

    Your commit message here

--- generateqr.py
+++ generateqr.py
@@ -9,14 +9,3 @@ def generate_qr(user_id):
 generate_qr("suba")
 generate_qr("swey")
 generate_qr("vino")
-import qrcode
-
-def generate_qr(user_id):
-    img = qrcode.make(user_id)
-    img.save(f"{user_id}_qr.png")
-    print(f"QR code saved as {user_id}_qr.png")
-
-generate_qr("suba")
-generate_qr("swey")
-generate_qr("vino")
-
